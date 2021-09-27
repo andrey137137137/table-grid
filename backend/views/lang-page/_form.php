3aa9e094149d29f439d5f455fabf8b2625b3889a
Bannikov Andrey <andrey27x777@gmail.com>
2021-09-28 01:10:15 +0300

    Added WYSIWYG

--- backend/views/lang-page/_form.php
+++ backend/views/lang-page/_form.php
@@ -2,6 +2,7 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use vova07\imperavi\Widget;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\LangPage */
@@ -18,7 +19,23 @@ use yii\widgets\ActiveForm;
 
   <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
 
-  <?= $form->field($model, 'text')->textarea(['rows' => 6]) ?>
+  <!-- ?= $form->field($model, 'text')->textarea(['rows' => 6]) ? -->
+  <?= $form->field($model, 'content')->widget(Widget::className(), [
+    'settings' => [
+      'lang' => 'ru',
+      'minHeight' => 200,
+      'plugins' => [
+        'clips',
+        'fullscreen',
+      ],
+      'clips' => [
+        ['Lorem ipsum...', 'Lorem...'],
+        ['red', '<span class="label-red">red</span>'],
+        ['green', '<span class="label-green">green</span>'],
+        ['blue', '<span class="label-blue">blue</span>'],
+      ],
+    ],
+  ]) ?>
 
   <?= $form->field($model, 'lang')->textInput(['maxlength' => true]) ?>
 
